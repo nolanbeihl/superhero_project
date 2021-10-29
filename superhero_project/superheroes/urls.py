606060e7b8d7e308faca7f57fe1bf0fa22e86ab7
unknown <nolan.beihl@gmail.com>
2021-10-29 07:58:26 -0600

    Start of day two on Superhero project

---
+++ superhero_project/superheroes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'superheroes'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail')
+]
+
