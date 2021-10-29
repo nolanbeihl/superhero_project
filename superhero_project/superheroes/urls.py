7f5bbc39a07c8e25f47ed70c9f788dfb687539d8
unknown <nolan.beihl@gmail.com>
2021-10-29 09:09:22 -0600

    Created update page and function,create page and function, as well as updated urls.py

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:hero_id>/', views.detail, name='detail')
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create'),
+    path('update/', views.update, name='update'),
+
+
 ]
 
