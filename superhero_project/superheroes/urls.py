37e2a085a0c946be43d76fcb726e7f6eee5fc355
unknown <nolan.beihl@gmail.com>
2021-10-29 16:01:27 -0600

    was able to get the remove function build as well as completed the update funciton.  Now the Details html is having errors, will have to address before submitting assignment

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>/', views.detail, name='detail'),
     path('new/', views.create, name='create'),
-    path('update/', views.update, name='update'),
-
+    path('<int:hero_id>', views.update, name='update'),
+    path('<int:hero_id>', views.delete, name='delete'),
 ]
 
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -32,7 +32,12 @@ def create(request):
     else:
         return render(request,'superheroes/create.html')
 
-def update(request):
+def update(request, hero_id):
+    update_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'update_hero': update_hero
+    }
+    #query for hero, pass it to context
     if request.method == "POST":
         name = request.POST.get('name')
         alter_ego = request.POST.get('alter_ego')
@@ -40,9 +45,24 @@ def update(request):
         secondary = request.POST.get('secondary')
         catchphrase = request.POST.get('catchphrase')
         update_hero = Superhero(name=name, alter_ego=alter_ego,primary_ability=primary,secondary_ability=secondary,catch_phrase=catchphrase)
-        update_hero.save()
-
+        request = update_hero
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
-        return render(request,'superheroes/update.html')
-
+        return render(request,'superheroes/update.html',context)
+        
+def delete(request,hero_id):
+    remove_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'remove_hero': remove_hero
+    }
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        remove_hero = Superhero(name=name, alter_ego=alter_ego,primary_ability=primary,secondary_ability=secondary,catch_phrase=catchphrase)
+        remove_hero.delete()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request,'superheroes/update.html',context)
