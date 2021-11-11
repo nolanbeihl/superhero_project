2ca01181a7f7c22dd0d729389872eaaf29a2514f
unknown <nolan.beihl@gmail.com>
2021-11-11 13:14:05 -0700

    Static image is set, working on animation and formatting of conten for index page and then will work on the rest of the pages.

--- superhero_project/superhero_project/settings.py
+++ superhero_project/superhero_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/superhero_project/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
--- superhero_project/superhero_project/urls.py
+++ superhero_project/superhero_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from superheroes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
